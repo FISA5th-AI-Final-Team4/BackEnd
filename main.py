5bc04200aec70171b697759e1a595eb282d3532c
ljh4770 <ljh4770@naver.com>
2025-10-22 22:17:07 +0900

    🌱 Add: 테스트용 /api/health 엔드포인트 추가
    - ./api/router.py의 api_router 구성
    - ./api/routes/ 디렉토리에 엔드포인트 구성 예정
    - ./api/routes/health.py에 테스트용 엔드포인트 추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from api.router import api_router
 
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {'message': "Woori FISA 5th - Team 4 Backend Server"}
+
+app.include_router(api_router, prefix='/api')
