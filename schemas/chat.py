b0c1e57bb44490cf948816e15c929aec788944d5
ljh4770 <ljh4770@naver.com>
2025-10-23 15:39:25 +0900

    ✨ Add: 채팅 관련 Schema 파일 초기화, 구현 필요

---
+++ schemas/chat.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class Persona(BaseModel):
+    pass
+
+class PeronaListResponse(BaseModel):
+    pass
+
+class ChatSessionRequest(BaseModel):
+    pass
+
+class ChatSessionResponse(BaseModel):
+    pass
+
+class ChatMessage(BaseModel):
+    pass
